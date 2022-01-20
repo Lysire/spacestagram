600eca965887cb5c9e24f83a6e12486cd80c5e84
Lysire <slackertux13.37@gmail.com>
2022-01-20 22:10:12 +0800

    netlify setup

--- src/api/download.js
+++ src/api/download.js
@@ -10,7 +10,7 @@ let [start, end] = getDateRange(offset, duration)
 // nasa api configurations
 const url = 'https://api.nasa.gov/planetary/apod'
 let params = {
-    api_key: "DEMO_KEY",
+    api_key: process.env.API_KEY,
     thumbs: true,
     start_date: start,
     end_date: end,
